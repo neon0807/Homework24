e5ce4920afc638100ec74ba2f6615e61899c30d2
comp <vitaliivorob3@gmail.com>
2023-03-03 15:14:27 +0300

    Homework24.1

--- src/main/java/skypro/controller/RestControllerNew.java
+++ src/main/java/skypro/controller/RestControllerNew.java
@@ -2,6 +2,7 @@ package skypro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import skypro.entity.Employee;
 import skypro.srvice.EmployeeService;
@@ -19,5 +20,10 @@ public class RestControllerNew {
     @GetMapping("/employee")
     public List<Employee> getEmployee() {
         return employeeService.getEmployee();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getPersonById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
     }
 }
