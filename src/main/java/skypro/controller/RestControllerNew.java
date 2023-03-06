aaacc937541e4f30ec9ab018f5cb2c58266d6821
comp <vitaliivorob3@gmail.com>
2023-03-06 11:00:31 +0300

    Homework24.2

--- src/main/java/skypro/controller/RestControllerNew.java
+++ src/main/java/skypro/controller/RestControllerNew.java
@@ -1,9 +1,7 @@
 package skypro.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import skypro.entity.Employee;
 import skypro.srvice.EmployeeService;
 
@@ -23,7 +21,31 @@ public class RestControllerNew {
     }
 
     @GetMapping("/employee/{id}")
-    public Employee getPersonById(@PathVariable int id) {
+    public Employee getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        employeeService.addEmployee(employee);
+        return employee;
+
+    }
+
+    @PutMapping("/employee")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.updateEmployee(employee);
+        return employee;
+
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
+
     }
 }
